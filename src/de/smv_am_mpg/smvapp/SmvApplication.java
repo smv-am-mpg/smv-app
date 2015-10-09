4815170798bfab1de7a625327fe1c5cec08dddd4
intrigus <abc123zeus@live.de>
2015-10-09 23:04:54 +0200

    Add Parse crash reporting, remove parse jar, use gradle for that

--- src/de/smv_am_mpg/smvapp/SmvApplication.java
+++ src/de/smv_am_mpg/smvapp/SmvApplication.java
@@ -3,12 +3,16 @@ package de.smv_am_mpg.smvapp;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseCrashReporting;
 
-public class SmvApplication extends Application{
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        Parse.initialize(this, "1aBJyf0oXUSrEKqDndGZYHFm3SnwUnXsMqN7PhyM",
-                "8NL6rHsi5Nh7hQVLB6smRoJRZqCOpwZVVnR6uBno");
-    }
+public class SmvApplication extends Application {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		// Enable Crash Reporting
+		ParseCrashReporting.enable(this);
+		// Setup Parse
+		Parse.initialize(this, "1aBJyf0oXUSrEKqDndGZYHFm3SnwUnXsMqN7PhyM",
+				"8NL6rHsi5Nh7hQVLB6smRoJRZqCOpwZVVnR6uBno");
+	}
 }
